b270bbe0271a56d42f0e842c3738602db7f8a725
ACoolName <acoolname9001@gmail.com>
2020-10-03 00:14:59 +0300

    fixed listed hooks checking and stringified ids, also made it so timestamp is optional

--- main.py
+++ main.py
@@ -48,24 +48,25 @@ class ACoolBot(commands.Bot):
 
         if 'write_embed' in actions:
             action = actions['write_embed']
-            channel = discord.utils.find(lambda c: c.id == action['channel'], message.guild.text_channels)
+            channel = discord.utils.find(lambda c: str(c.id) == action['channel'], message.guild.text_channels)
             if channel is None:
                 return
 
             namespace = {'author': message.author,
                          'created_at': message.created_at.strftime('%y/%m/%d %H:%M:%S'),
-                         'edited_at': message.edited_at.strftime('%y/%m/%d %H:%M:%S'),
+                         'edited_at': message.edited_at.strftime('%y/%m/%d %H:%M:%S') if message.edited_at is not None else '',
                          'content': message.content,
                          'attachments': message.attachments,
                          'message_id': message.id,
-                         'jump_url': message.jump_url}
+                         'jump_url': message.jump_url,
+                         'author_mention': message.author.mention}
 
             embed = action['embed']
 
-            for field, value in embed.copy().iteritems():
+            for field, value in embed.copy().items():
                 embed[field] = value.format(**namespace)
 
-            if embed['timestamp']:
+            if 'timestamp' in embed:
                 embed['timestamp'] = datetime.strptime(embed['timestamp'], '%y/%m/%d %H:%M:%S')
 
             embed = discord.Embed(**embed)
@@ -75,8 +76,9 @@ class ACoolBot(commands.Bot):
                 embed.set_image(url=action['image'])
             if 'thumbnail' in action:
                 embed.set_thumbnail(url=action['thumbnail'])
-            for field in action['fields']:
-                embed.add_field(**field)
+            if 'fields' in action:
+                for field in action['fields']:
+                    embed.add_field(**field)
 
             await channel.send(embed=embed)
 
@@ -88,19 +90,19 @@ class ACoolBot(commands.Bot):
         :return:
         """
         if "contains" in hook:
-            if not ((hook['contains'][0] in message.content) ^ hook['contains'][1]):
+            if not (any(map(lambda s: s in message.content, hook['contains'][0])) ^ hook['contains'][1]):
                 return False
         if 'author' in hook:
-            if not (message.author.id in hook['author'][0] ^ hook['author'][1]):
+            if not ((str(message.author.id) in hook['author'][0]) ^ hook['author'][1]):
                 return False
         if 'roles' in hook:
-            if not (any(map(lambda r: r.id in hook['roles'][0], message.author.roles)) ^ hook['roles'][1]):
+            if not (any(map(lambda r: str(r.id) in hook['roles'][0], message.author.roles)) ^ hook['roles'][1]):
                 return False
         if 'channels' in hook:
-            if not (message.channel in hook['channels'][0] ^ hook['channels'][1]):
+            if not ((str(message.channel.id) in hook['channels'][0]) ^ hook['channels'][1]):
                 return False
         if 'attachments' in hook:
-            if not (len(message.attachments) ^ hook['attachments']):
+            if not ((len(message.attachments) > 0) ^ hook['attachments']):
                 return False
         return True
 
