5a14facf88a923e6f4874291f407417728df2638
ACoolUsername9001 <acoolname9001@gmail.com>
2021-01-02 16:14:24 +0200

    made ending groups anonymous

--- utilities/regex.py
+++ utilities/regex.py
@@ -1,3 +1,3 @@
 import re
 
-website_image_regex = re.compile(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)\.((gif)|(png)|(jpg))")
+website_image_regex = re.compile(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)\.(?:(?:gif)|(?:png)|(?:jpg))")
